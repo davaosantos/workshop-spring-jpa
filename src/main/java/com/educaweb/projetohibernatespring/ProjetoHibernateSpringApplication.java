e7777d4a863d8e7f87e76de06d2903d8ad52da36
david <david_Alisson60@yahoo.com.br>
2024-09-04 21:15:23 -0300

    first commit

---
+++ src/main/java/com/educaweb/projetohibernatespring/ProjetoHibernateSpringApplication.java
@@ -0,0 +1,13 @@
+package com.educaweb.projetohibernatespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoHibernateSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoHibernateSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/educaweb/projetohibernatespring/ProjetoHibernateSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educaweb.projetohibernatespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoHibernateSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
