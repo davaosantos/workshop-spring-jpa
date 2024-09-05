09777f6a5b4f3535b624b5961a397e2e7e3a85b6
david <david_Alisson60@yahoo.com.br>
2024-09-04 21:44:49 -0300

    301 - H2 e Test profile

--- src/main/java/com/educaweb/projetohibernatespring/entities/User.java
+++ src/main/java/com/educaweb/projetohibernatespring/entities/User.java
@@ -1,11 +1,17 @@
 package com.educaweb.projetohibernatespring.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serial;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
