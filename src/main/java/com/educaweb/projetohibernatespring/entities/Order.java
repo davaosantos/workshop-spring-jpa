033cbc7eff326149cdb2f9e301e05d7305c5dd1c
david <david_Alisson60@yahoo.com.br>
2024-09-24 21:48:58 -0300

    S23 - 273 - Order Status enum

--- src/main/java/com/educaweb/projetohibernatespring/entities/Order.java
+++ src/main/java/com/educaweb/projetohibernatespring/entities/Order.java
@@ -1,5 +1,6 @@
 package com.educaweb.projetohibernatespring.entities;
 
+import com.educaweb.projetohibernatespring.entities.enuns.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
@@ -23,13 +24,16 @@ public class Order implements Serializable {
     @JoinColumn(name = "client_id")
     private User client;
 
+    private Integer orderStatus;
+
     public Order() {
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, User client, OrderStatus orderStatus) {
         this.id = id;
         this.moment = moment;
         this.client = client;
+        setOrderStatus(orderStatus);
     }
 
     public Long getId() {
@@ -54,6 +58,18 @@ public class Order implements Serializable {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public OrderStatus getOrderStatus(Integer orderStatus) {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+
+        if (orderStatus != null){
+            this.orderStatus = orderStatus.getCode();
+        }
+
     }
 
     @Override
