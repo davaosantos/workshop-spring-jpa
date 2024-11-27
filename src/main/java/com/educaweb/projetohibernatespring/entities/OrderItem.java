71421df15bfce3a940b227b2c518e9d16029f620
david <david_Alisson60@yahoo.com.br>
2024-11-27 13:05:35 -0300

    S23 - 277. Entidade OrderItem. Associação muitos-para-muitos com dados extras - PARTE 1

---
+++ src/main/java/com/educaweb/projetohibernatespring/entities/OrderItem.java
@@ -0,0 +1,89 @@
+package com.educaweb.projetohibernatespring.entities;
+
+import com.educaweb.projetohibernatespring.entities.pk.OrderItemPK;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+
+@Entity
+@Table(name = "tb_order_item")
+public class OrderItem implements Serializable {
+
+    @EmbeddedId
+    private OrderItemPK id;
+
+    private Integer quantity;
+
+    private Double price;
+
+    public OrderItem() {
+    }
+
+    public OrderItem(Order order, Product product, Integer quantity, Double price) {
+        super();
+        this.getId().setOrder(order);
+        this.getId().setProduct(product);
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public OrderItemPK getId() {
+        return id;
+    }
+
+    public void setId(OrderItemPK id) {
+        this.id = id;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Order getOrder(){
+        return this.getId().getOrder();
+    }
+
+
+
+
+    public Product getProduct(){
+        return this.getId().getProduct();
+    }
+
+    public void setOrder(Order order){
+        this.id.setOrder(order);
+    }
+
+    public void setProduct(Product product){
+        this.id.setProduct(product);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderItem orderItem = (OrderItem) o;
+        return Objects.equals(id, orderItem.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
